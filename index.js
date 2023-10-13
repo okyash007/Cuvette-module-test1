cf9b3eabddeceab3b3771bc816f2d4c793efbd13
Yash Verma <helloyashverma@gmail.com>
2023-10-13 09:19:31 +0530

    some css added

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const buttons = document.querySelectorAll("button");
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+const buttons = [rock , paper , scissors]
 const resultArea = document.querySelector("#result_area");
-const playetElement = document.getElementById('player')
+const playerElement = document.getElementById('player')
 const computerElement = document.getElementById('computer')
+const rulesBTN = document.querySelector('#rules div button')
 let playerCount = localStorage.getItem('playerCount') || 0;
 let computerCount = localStorage.getItem('computerCount') || 0;
+
+playerElement.innerHTML = playerCount
+computerElement.innerHTML = computerCount
+
+rock.id = "rock"
+paper.id = "paper"
+scissors.id = "scissors"
+
+console.log(buttons)
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
@@ -27,31 +40,50 @@ function computerPlay() {
 
 let computer = computerPlay();
 
-const button = document.createElement("button");
-button.innerText = "try again";
-button.addEventListener("click", () => {
+const tryAgainButton = document.createElement("button");
+tryAgainButton.innerText = "PLAY AGAIN";
+tryAgainButton.classList.add("try-again")
+tryAgainButton.addEventListener("click", () => {
   window.location.reload();
 });
 
+const textAgainstPC = document.createElement("p")
+textAgainstPC.innerHTML = "AGAINST PC"
+
+const result = document.createElement("p")
+
+const resultArea_div = document.createElement("div")
+resultArea_div.classList.add("resultArea-div")
+resultArea_div.appendChild(result)
+resultArea_div.appendChild(textAgainstPC)
+resultArea_div.appendChild(tryAgainButton)
+
+
+// rulesBTN.addEventListener('click',  )
+
+
+
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    let result = playRound(buttons[i].innerHTML, computer);
-    resultArea.innerHTML = result;
-    if(result === "You win!" ){
+    // console.log("hi")
+    let resultText = playRound(buttons[i].id, computer);
+    result.innerHTML = resultText;
+    if(resultText === "You win!" ){
       playerCount++
       localStorage.setItem('playerCount', playerCount);
-      playetElement.innerHTML = localStorage.getItem('playerCount')
+      playerElement.innerHTML = localStorage.getItem('playerCount')
     }
-    else if(result === "You lose!") {
+    else if(resultText === "You lose!") {
       computerCount++
       localStorage.setItem('computerCount', computerCount);
       computerElement.innerHTML = localStorage.getItem('computerCount')
     }
-    resultArea.appendChild(button);
+    resultArea.innerHTML = ""
+    resultArea.appendChild(resultArea_div)
   });
 }
 
-playetElement.innerHTML = localStorage.getItem('playerCount')
-computerElement.innerHTML = localStorage.getItem('computerCount')
+
+
 
 console.log(localStorage)
