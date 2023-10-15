edecc91a7e76c6b37c63404f73575f68c9b12865
Yash Verma <helloyashverma@gmail.com>
2023-10-15 16:50:52 +0530

    next page added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const body = document.querySelector(".body");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
@@ -22,15 +23,15 @@ console.log(buttons);
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a tie!";
+    return "TIE";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return "You win!";
+    return "YOU WIN";
   } else {
-    return "You lose!";
+    return "YOU LOSE";
   }
 }
 
@@ -70,6 +71,11 @@ const rules = [
 ];
 
 const ul = document.createElement("ul");
+
+const ulHeading = document.createElement("h1");
+ulHeading.innerHTML = "Game Rules";
+ul.appendChild(ulHeading);
+
 for (let i = 0; i < rules.length; i++) {
   const li = document.createElement("li");
   li.textContent = rules[i];
@@ -77,25 +83,56 @@ for (let i = 0; i < rules.length; i++) {
 }
 
 const ulClose = document.createElement("button");
-ulClose.innerHTML = "X"
+ulClose.innerHTML = "X";
 ulClose.addEventListener("click", function () {
   rulesTextDiv.innerHTML = "";
 });
 
 rulesBTN.addEventListener("click", function () {
-  rulesTextDiv.appendChild(ulClose)
+  rulesTextDiv.appendChild(ulClose);
   rulesTextDiv.appendChild(ul);
 });
-
-
 
 const winnerDiv = document.createElement("div");
 winnerDiv.classList.add("winnerDiv");
 const loserDiv = document.createElement("div");
 loserDiv.classList.add("loserDiv");
+
 const nextBTN = document.createElement("button");
 nextBTN.classList.add("btn");
 nextBTN.innerHTML = "NEXT";
+
+const nextDIV = document.createElement("div");
+nextDIV.classList.add('nextDIV')
+const nextIMG = document.createElement("div");
+nextIMG.classList.add('nextIMG')
+const stars = document.createElement("img");
+stars.classList.add('stars')
+stars.src = "./Group 5.svg";
+
+const trophy = document.createElement("img")
+trophy.classList.add("trophy")
+trophy.src = "./Vector.svg"
+
+nextIMG.appendChild(stars);
+nextIMG.appendChild(trophy)
+const tryAgainButtonCopy = tryAgainButton.cloneNode(true);
+tryAgainButtonCopy.addEventListener("click", () => {
+  window.location.reload(tryAgainButtonCopy);
+});
+
+const hurry = document.createElement('h1')
+hurry.classList.add('hurry')
+hurry.innerHTML = "HURRY!!"
+
+const youWonText = document.createElement('h1')
+youWonText.classList.add('youWonText')
+youWonText.innerHTML = "YOU WON THE GAME"
+
+nextDIV.appendChild(nextIMG);
+nextDIV.appendChild(hurry)
+nextDIV.appendChild(youWonText)
+nextDIV.appendChild(tryAgainButtonCopy);
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
@@ -103,14 +140,14 @@ for (let i = 0; i < buttons.length; i++) {
     result.innerHTML = resultText;
     console.log(buttons[i]);
     console.log(computer);
-    if (resultText === "You win!") {
+    if (resultText === "YOU WIN") {
       rulesBtnDiv.appendChild(nextBTN);
       winnerDiv.classList.add("wins");
       playerCount++;
       localStorage.setItem("playerCount", playerCount);
       playerElement.innerHTML = localStorage.getItem("playerCount");
     }
-    if (resultText === "You lose!") {
+    if (resultText === "YOU LOSE") {
       loserDiv.classList.add("wins");
       computerCount++;
       localStorage.setItem("computerCount", computerCount);
@@ -125,5 +162,12 @@ for (let i = 0; i < buttons.length; i++) {
     resultArea.appendChild(loserDiv);
   });
 }
+
+nextBTN.addEventListener("click", function () {
+  body.innerHTML = " ";
+  body.appendChild(nextDIV);
+  nextBTN.remove(rulesBtnDiv);
+  console.log(body);
+});
 
 console.log(localStorage);
